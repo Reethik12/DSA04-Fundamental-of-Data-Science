0f24ec71596e46e2732c0b4fb861261bc0ba8206
Reethik12 <192473006.simats@saveetha.com>
2025-12-18 09:32:46 +0530

    Add files via upload

---
+++ 10. monthly_sales_visualization.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+df=pd.read_csv('Sales_data.csv')
+monthly_sales=df.groupby("Month_sales")["Price"].sum()
+
+plt.plot(monthly_sales.index, monthly_sales.values, marker='o')
+plt.title("Monthly Sales Data")
+plt.xlabel("Month")
+plt.ylabel("Total Sales")
+plt.show()
+
+plt.bar(monthly_sales.index, monthly_sales.values)
+plt.title("Monthly Sales Data")
+plt.xlabel("Month")
+plt.ylabel("Total Sales")
+plt.show()
